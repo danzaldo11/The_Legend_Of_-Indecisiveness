cc5253a1b843689c8393d52e3d978b5295b50e52
marianal13 <98548870+marianal13@users.noreply.github.com>
2022-03-22 17:01:31 -0700

    Add files via upload

---
+++ mlara2.cpp
@@ -0,0 +1,68 @@
+// Author: Mariana Lara
+// Project Software Eng.
+//
+// 
+
+//Websites used:
+//Character: https://www.pinterest.ie/pin/257338566191273090/
+//Character: https://www.pngegg.com/en/png-eticx
+
+//Map: https://www.deviantart.com/erichkisaragi/art/Beach-House-604911358
+
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+//#include "credits.h"
+
+//Mouse and keyboard
+//Credits
+
+
+//Run Map and Character
+class Image {
+public:
+	int width, height;
+	unsigned char *data;
+	~Image() { delete [] data; }
+	Image(const char *fname) {
+		if (fname[0] == '\0')
+			return;
+		//printf("fname **%s**\n", fname);
+		char name[40];
+		strcpy(name, fname);
+		int slen = strlen(name);
+		name[slen-4] = '\0';
+		//printf("name **%s**\n", name);
+		char ppmname[80];
+		sprintf(ppmname,"%s.ppm", name);
+		//printf("ppmname **%s**\n", ppmname);
+		char ts[100];
+		//system("convert eball.jpg eball.ppm");
+		sprintf(ts, "convert %s %s", fname, ppmname);
+		system(ts);
+		//sprintf(ts, "%s", name);
+		FILE *fpi = fopen(ppmname, "r");
+		if (fpi) {
+			char line[200];
+			fgets(line, 200, fpi);
+			fgets(line, 200, fpi);
+			while (line[0] == '#')
+				fgets(line, 200, fpi);
+			sscanf(line, "%i %i", &width, &height);
+			fgets(line, 200, fpi);
+			//get pixel data
+			int n = width * height * 3;			
+			data = new unsigned char[n];			
+			for (int i=0; i<n; i++)
+				data[i] = fgetc(fpi);
+			fclose(fpi);
+		} else {
+			printf("ERROR opening image: %s\n",ppmname);
+			exit(0);
+		}
+		unlink(ppmname);
+	}
+};
+Image img[2] = {"girl_char.gif",
+                 "beach_map"};
+
