cec15a819c442dfbfedd4dae3a8f8a579440a806
danzaldo11 <95332579+danzaldo11@users.noreply.github.com>
2022-04-04 10:25:10 -0700

    gameover_function

---
+++ danzaldo.cpp
@@ -0,0 +1,34 @@
+//3350
+//program: danzaldo.cpp
+//author:  Dylan Anzaldo
+//date:    spring 2022
+//
+//Medieval map and sprite movement
+//images courtesy: https://www.artstation.com/artwork/xJv4WW
+//                 https://www.artstation.com/artwork/PmggkZ 
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+
+void danzaldo_gameover(int health) {
+    if (health <= 0) {
+        printf("You Died!\n");
+    } else {
+        printf("Health: %i\n", health);
+    }   
+}
+
+int main() {
+    /* int health = 100;
+    danzaldo_gameover(health);
+    health = 25;
+    danzaldo_gameover(health);
+    health = 0;
+    danzaldo_gameover(health); */
+
+    return 0;
+}
