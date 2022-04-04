155101fb38035aeda0f16e744f41b91aebadfcd3
madstyner <90936039+madstyner@users.noreply.github.com>
2022-04-04 10:50:30 -0700

    Add files via upload

--- msteiner.cpp
+++ msteiner.cpp
@@ -2,9 +2,6 @@
 // CMPS 3350
 //
 
-// Map: https://graphicriver.img.customer.envatousercontent.com/files/194999057/previewl.jpg?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=590&s=4ffbcf15824c22e1a243c8a3f0dfd265
-// Character: https://www.pinterest.com/pin/361836151308159517/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,62 +9,26 @@
 #include <time.h>
 #include <math.h>
 
-class Image {
-    public:
-        int width, height;
-        unsigned char *data;
-        ~Image() { delete [] data; }
-        Image(const char *fname) {
-            if (fname[0] == '\0')
-                return;
-            printf("fname **%s**\n", fname);
-            int ppmFlag = 0;
-            char name[40];
-            strcpy(name, fname);
-            int slen = strlen(name);
-            char ppmname[80];
-            if (strncmp(name+(slen-4), ".ppm", 4) == 0)
-                ppmFlag = 1;
-            if (ppmFlag) {
-                strcpy(ppmname, name);
-            } else {
-                name[slen-4] = '\0';
-                //printf("name **%s**\n", name);
-                sprintf(ppmname,"%s.ppm", name);
-                //printf("ppmname **%s**\n", ppmname);
-                char ts[100];
-                //system("convert eball.jpg eball.ppm");
-                sprintf(ts, "convert %s %s", fname, ppmname);
-                system(ts);
-            }
-            sprintf(ts, "%s", name);
-            printf("read ppm **%s**\n", ppmname); fflush(stdout);
-            FILE *fpi = fopen(ppmname, "r");
-            if (fpi) {
-                char line[200];
-                fgets(line, 200, fpi);
-                fgets(line, 200, fpi);
-                //skip comments and blank lines
-                while (line[0] == '#' || strlen(line) < 2)
-                    fgets(line, 200, fpi);
-                sscanf(line, "%i %i", &width, &height);
-                fgets(line, 200, fpi);
-                //get pixel data
-                int n = width * height * 3;
-                data = new unsigned char[n];
-                for (int i=0; i<n; i++)
-                    data[i] = fgetc(fpi);
-                fclose(fpi);
-            } else {
-                printf("ERROR opening image: %s\n",ppmname);
-                exit(0);
-            }
-            if (!ppmFlag)
-                unlink(ppmname);
-        }
-};
+void msteiner_Check_Stamina(int stamina)
+{
+    //stamina = 100;
 
+    if (stamina <= 0)
+    {
+        printf("You are out of breath, take a walk to heal.\n");
+        stamina++;
+    }
+    else
+    {
+        printf("Stamina: %i\n", stamina);
+        printf("Keep Running!\n");
+        stamina --;
+    }
+}
 
-Image img[1] = {
-   "housemap",
-   "girlwalk.gif" };
+int main()
+{
+    msteiner_Check_Stamina(100);
+
+    return 0;
+}
