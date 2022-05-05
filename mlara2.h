7c0c097c81aeacaf70ea80326b3ad7ec5037e64c
marianal13 <98548870+marianal13@users.noreply.github.com>
2022-05-05 09:42:19 -0700

    Update mlara2.h

--- mlara2.h
+++ mlara2.h
@@ -1,3 +1,13 @@
 //header file for mlara2.cpp
 
 extern void mlara2_armour(int a);
+extern void sprite_move_right2();
+extern void sprite_move_left2();
+extern void sprite_move_down2();
+extern void sprite_move_up2();
+extern void select_start_screen2();
+extern void render_start_screen2();
+extern void init_start_screen2();
+extern void level_select_screen2();
+extern void physics_level_two();
+
