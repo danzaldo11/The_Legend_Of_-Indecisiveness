2c63851b6c9aaab2a7d1612a03a7d2a82f125009
Gisela Jimenez Rojas <93414079+giselajimenezr@users.noreply.github.com>
2022-05-04 23:59:12 -0700

    Update gjimenezroja.h

--- gjimenezroja.h
+++ gjimenezroja.h
@@ -1,3 +1,11 @@
 //header file
 
-extern void gjimenezroja(int inventory); 
+//extern void gjimenezroja(int inventory); 
+
+extern void select_level_two();
+
+extern void physics_level_two(); 
+
+extern void render_level_two(); 
+
+extern void init_level_two(); 
