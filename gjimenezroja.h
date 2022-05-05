c848aa8be964642173847752a93d07d62d9fe0c7
Gisela Jimenez Rojas <93414079+giselajimenezr@users.noreply.github.com>
2022-05-05 03:40:02 -0700

    Update gjimenezroja.h

--- gjimenezroja.h
+++ gjimenezroja.h
@@ -1,4 +1,21 @@
 //header file
 
-//extern void gjimenezroja(int inventory); 
+//extern void gjimenezroja(int inventory);
 
+extern void select_level_three();
+
+extern void physics_level_three(); 
+
+extern void render_level_three(); 
+
+extern void init_level_three(); 
+
+extern void level_select_screens(); 
+
+extern void ssprite_move_right();
+extern void ssprite_move_left();
+extern void ssprite_move_up();
+extern void ssprite_move_down();
+extern void select_start_screens(); 
+extern void render_start_screens(); 
+extern void init_start_screens(); 
