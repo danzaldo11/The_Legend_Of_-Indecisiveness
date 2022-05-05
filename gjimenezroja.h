27675ef2731da8ec4baab7b11c79a80d1ba1859b
Gisela Jimenez Rojas <93414079+giselajimenezr@users.noreply.github.com>
2022-05-05 00:19:32 -0700

    Update gjimenezroja.h

--- gjimenezroja.h
+++ gjimenezroja.h
@@ -2,10 +2,3 @@
 
 //extern void gjimenezroja(int inventory); 
 
-extern void select_level_three();
-
-extern void physics_level_three(); 
-
-extern void render_level_three(); 
-
-extern void init_level_three(); 
