a9ee127a5cccabec8f9b96bd1f483a92765a0744
madstyner <90936039+madstyner@users.noreply.github.com>
2022-05-05 09:08:43 -0700

    Update msteiner.h

--- msteiner.h
+++ msteiner.h
@@ -1,2 +1,17 @@
 // Header file - msteiner.cpp
-extern void msteiner_Check_Stamina(int stamina);
+
+extern void select_level_four();
+extern void physics_level_four(); 
+extern void render_level_four(); 
+extern void init_level_four(); 
+extern void level_select_screens(); 
+extern void ssprite_move_right();
+extern void ssprite_move_left();
+extern void ssprite_move_up();
+extern void ssprite_move_down();
+extern void select_start_screens(); 
+extern void render_start_screens(); 
+extern void init_start_screens(); 
+
+
+// extern void msteiner_Check_Stamina(int stamina);
